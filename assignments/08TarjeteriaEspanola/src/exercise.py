5451fd73ee789254593052ec44907fa3b734ab6f
KarenP1525 <89099490+KarenP1525@users.noreply.github.com>
2021-08-30 21:38:05 -0500

    Update exercise.py
    TarjetasMax

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,6 +1,22 @@
 
+def maxTe(papeles,plumones):
+    totPapTar=papeles*12
+    totPluTar=plumones*35
+    if(totPapTar==totPluTar):
+        r=totPluTar
+    elif(totPapTar<totPluTar):
+        r=totPapTar
+    else:
+        r=totPluTar
+    return r
+
 def main():
     #escribe tu código abajo de esta línea
+    papeles=int(input("Dame la cantidad de pliegos de papel albanene: "))
+    plumones=int(input("Dame la cantidad de plumones: "))
+    
+    totTarjetas=maxTe(papeles,plumones)
+    print("El número máximo de tarjetas que se pueden hacer es:",totTarjetas)
     pass
 
 if __name__=='__main__':
