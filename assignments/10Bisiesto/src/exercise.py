b3f3aecb41d28dfabd54dd48ae77bd0b7b582920
KarenP1525 <89099490+KarenP1525@users.noreply.github.com>
2021-08-30 23:31:08 -0500

    Update exercise.py
    bisiesto_true_false

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,13 @@
 
+def es_bisiesto(anio):
+    return not anio%4 and ((not anio%100 and not anio%400)or(anio%100!=0))
+
+
 def main():
     #escribe tu código abajo de esta línea
+    anio=int(input("Dame el año: "))
+    print(es_bisiesto(anio))
+
     pass
 
 if __name__=='__main__':
